b07e9470f474c25d496938f5adcfb36b91d94239
Student <hsmo1501@mail.ru>
2020-09-22 09:14:14 +0500

    добавлены изменения

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -4,4 +4,14 @@ import (
 	"github.com/hsmo1501/bankapp/pkg/bank/types"
 )
 
-im
+func Avg(payments []types.Payment)  types.Money{
+	 summ := types.Money(0)
+	 for _, payment := range payments{
+		
+		 if payment.Amount > 0 {
+			 summ += summ + payment.Amount
+		 }
+	 }
+	average := summ / types.Money(len(payments))
+	return average 
+}
